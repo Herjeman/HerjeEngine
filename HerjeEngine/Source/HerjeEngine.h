1bf9005473e2165e8619280b939723a60e2e757c
GustavEnglund <englundgustav@gmail.com>
2024-05-19 17:39:17 +0200

    Addidtions to include .h file

--- HerjeEngine/Source/HerjeEngine.h
+++ HerjeEngine/Source/HerjeEngine.h
@@ -4,10 +4,12 @@
 #include <functional>
 #include <map>
 #include <vector>
+#include <memory>
 
 // For use by HE applications
 #include "Engine/Core/Application.h"
 #include "Engine/Log/Log.h"
+#include "Utilities/Vector2.h"
 
 // Entry point
 #include "Engine/Core/EntryPoint.h"
